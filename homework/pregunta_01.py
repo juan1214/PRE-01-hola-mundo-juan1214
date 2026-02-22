df08383b0c2a01c3d398ceb495ac5b50582e9e75
Juan Manuel Sánchez Restrepo <66439221+juan1214@users.noreply.github.com>
2026-02-22 17:54:28 +0000

    Solucion Hola mundo

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,6 +5,8 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 # pylint: disable=import-outside-toplevel
 
 
+
+#funcion que retorna las dos palabras concatenadas
 def pregunta_01():
     """
     Retorne el string "Hola mundo cruel!".
@@ -13,8 +15,13 @@ def pregunta_01():
     Hola mundo cruel!
 
     """
+    #funcion para concatenar dos palabras
+    def saludo(palabra1, palabra2):
+        return f"{palabra1} {palabra2}!"
 
-    return
+    mensaje = saludo("Hola", "mundo cruel")
+
+    return mensaje
 
 
 if __name__ == "__main__":
--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -5,6 +5,8 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 # pylint: disable=import-outside-toplevel
 
 
+
+
 def pregunta_02():
     """
     Retorne el string "Hello cruel world!".
@@ -14,7 +16,13 @@ def pregunta_02():
 
     """
 
-    return
+    #funcion para concatenar dos palabras
+    def saludo2(palabra1, palabra2):
+        return f"{palabra1} {palabra2}!"
+
+    mensaje2 = saludo2("Hello", "cruel world")
+
+    return mensaje2
 
 
 if __name__ == "__main__":
